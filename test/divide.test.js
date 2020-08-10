3b244ccd68795d0cf5ab8685730ef6c13e190a03
Gavin Grant <gavingrant@Gavins-MacBook-Pro.local>
2020-08-09 18:19:53 -0700

    installed chai and added testing specs

--- test/divide.test.js
+++ test/divide.test.js
@@ -1,11 +1,30 @@
 const divide = require('../index');
+const expect = require('chai').expect;
 
 describe('Divide function', () => {
     it('should divide positive integers correctly', () => {
-        // spec code here
+        // define inputs
+        const a = 8;
+        const b = 4;
+        const expectedAnswer = 2;
+
+        //invoke the function
+        const actualAnswer = divide(a, b);
+
+        // assert that expected === actual
+        expect(actualAnswer).to.equal(expectedAnswer);
     })
 
     it('should throw an error when divided by zero', () => {
-        // spec code here
+        // define inputs
+        const a = 8, b = 0;
+
+        // set up the function call
+        const fn = () => {
+            divide (a, b)
+        };
+
+        // assert that exception is thrown
+        expect(fn).to.throw();
     })
 })
