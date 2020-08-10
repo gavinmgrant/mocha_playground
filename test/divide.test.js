77fd25f37b61ebedb88b7ee29c9570bf95e267f3
Gavin Grant <gavingrant@Gavins-MacBook-Pro.local>
2020-08-09 18:03:35 -0700

    initial commit with first test

---
+++ test/divide.test.js
@@ -0,0 +1,11 @@
+const divide = require('../index');
+
+describe('Divide function', () => {
+    it('should divide positive integers correctly', () => {
+        // spec code here
+    })
+
+    it('should throw an error when divided by zero', () => {
+        // spec code here
+    })
+})
